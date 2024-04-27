b5075f89d9a10664aa5ff3e6d38f332a92f017c0
BabkenK <139691909+BabkenK@users.noreply.github.com>
2024-04-27 14:08:41 +0000

    Update count-characters.cpp

--- count-characters.cpp
+++ count-characters.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
-int main(){
-std::string str = " enter your text";
-std::getline(std::cin, str);
-std::cout<<" The total number of characters is "<<str.size()<<std::endl;
-return 0;
+// Ձևակերպում ենք ֆունկցիա հաշվելու համար characters առանց բացատների
+int countcharacterswithoutspace(const std::string& str) {
+    int count = 0; //արժեքավորում ենք
+    for (char c : str) {
+        // եթե character չի հանդիսանում բացատ, increment ենք կատարում
+        if (c != ' ') {
+            count++;
+        }
+    }
+
+    return count;
+}
+int main() {
+    std::string str; // ձևակերպում ենք փոփոխական string
+    std::cout << "Enter your text: ";
+    std::getline(std::cin, str); // ստանում ենք օգտատիրոջ կողմից տեքստը
+
+    // ֆունկցիաի կանչ ենք իրականացնում
+    std::cout << "The total number of non-space characters is " << countcharacterswithoutspace(str) << std::endl;
+    return 0;
 }
